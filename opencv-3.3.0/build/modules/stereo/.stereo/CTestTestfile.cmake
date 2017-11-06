53e3287d72f991e19fe2d34a7a6eb146e28d1adf
pigeostake <cstarkjp@gmail.com>
2017-11-06 13:45:11 +0900

    Partial commit: stereo

---
+++ opencv-3.3.0/build/modules/stereo/.stereo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Packages/opencv_contrib-3.3.0/modules/stereo
+# Build directory: /home/pi/Packages/opencv-3.3.0/build/modules/stereo/.stereo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
