eac8b9125abd8e03e9e5895072cf3b883d02b371
pigeostake <cstarkjp@gmail.com>
2017-11-06 12:39:33 +0900

    Partial commit: imgcodecs

---
+++ opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/test/test_grfmt.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/test/test_grfmt.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/test/test_jpeg.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/test/test_jpeg.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/test/test_main.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/test/test_main.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/test/test_png.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/test/test_png.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/test/test_read_write.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/test/test_read_write.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/test/test_tiff.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/test/test_tiff.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/test/test_webp.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_test_imgcodecs.dir/test/test_webp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_WEBP"
+  "_USE_MATH_DEFINES"
+  "__OPENCV_BUILD=1"
+  "__OPENCV_TESTS=1"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../3rdparty/libwebp"
+  "."
+  "/usr/include/eigen3"
+  "/usr/include/arm-linux-gnueabihf"
+  "../3rdparty/openexr/Half"
+  "../3rdparty/openexr/Iex"
+  "../3rdparty/openexr/IlmThread"
+  "../3rdparty/openexr/Imath"
+  "../3rdparty/openexr/IlmImf"
+  "../modules/ts/include"
+  "../modules/imgcodecs/include"
+  "../modules/videoio/include"
+  "../modules/core/include"
+  "../modules/imgproc/include"
+  "../modules/highgui/include"
+  "../modules/imgcodecs/test"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Packages/opencv-3.3.0/build/modules/ts/CMakeFiles/opencv_ts.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/highgui/CMakeFiles/opencv_highgui.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_videoio.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_imgcodecs.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/core/CMakeFiles/opencv_core.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/3rdparty/carotene/hal/CMakeFiles/tegra_hal.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
