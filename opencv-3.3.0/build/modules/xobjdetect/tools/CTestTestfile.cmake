b75b069eac79a2a829b903358094a5a4d4a939af
pigeostake <cstarkjp@gmail.com>
2017-11-06 14:43:51 +0900

    Partial commit: xobjdetect

---
+++ opencv-3.3.0/build/modules/xobjdetect/tools/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Packages/opencv_contrib-3.3.0/modules/xobjdetect/tools
+# Build directory: /home/pi/Packages/opencv-3.3.0/build/modules/xobjdetect/tools
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("waldboost_detector")
