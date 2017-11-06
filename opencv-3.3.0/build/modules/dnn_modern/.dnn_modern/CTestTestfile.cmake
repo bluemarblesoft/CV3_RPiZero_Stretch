73b9965472dc9749758676f0657b26d98bb461b9
pigeostake <cstarkjp@gmail.com>
2017-11-06 12:16:53 +0900

    Partial commit: dnn_modern

---
+++ opencv-3.3.0/build/modules/dnn_modern/.dnn_modern/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Packages/opencv_contrib-3.3.0/modules/dnn_modern
+# Build directory: /home/pi/Packages/opencv-3.3.0/build/modules/dnn_modern/.dnn_modern
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
