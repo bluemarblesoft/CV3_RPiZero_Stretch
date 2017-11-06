abff7d37d2f3c024e4de715714727bc6b53ec9dd
pigeostake <cstarkjp@gmail.com>
2017-11-06 12:09:28 +0900

    Partial commit: cudaimgproc

---
+++ opencv-3.3.0/build/modules/cudaimgproc/.cudaimgproc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Packages/opencv-3.3.0/modules/cudaimgproc
+# Build directory: /home/pi/Packages/opencv-3.3.0/build/modules/cudaimgproc/.cudaimgproc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
