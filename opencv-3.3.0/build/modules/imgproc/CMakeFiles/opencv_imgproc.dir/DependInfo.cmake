36691eb41919618385950ce3df3555e38f3440cd
pigeostake <cstarkjp@gmail.com>
2017-11-06 12:48:47 +0900

    Partial commit: imgproc

---
+++ opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/DependInfo.cmake
@@ -0,0 +1,97 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/opencl_kernels_imgproc.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/opencl_kernels_imgproc.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/accum.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/accum.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/accum.dispatch.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/accum.dispatch.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/approx.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/approx.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/blend.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/blend.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/canny.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/canny.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/clahe.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/clahe.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/color.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/color.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/colormap.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/colormap.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/connectedcomponents.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/connectedcomponents.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/contours.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/contours.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/convhull.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/convhull.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/corner.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/corner.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/cornersubpix.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/cornersubpix.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/demosaicing.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/demosaicing.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/deriv.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/deriv.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/distransform.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/distransform.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/drawing.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/drawing.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/emd.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/emd.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/featureselect.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/featureselect.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/filter.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/filter.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/floodfill.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/floodfill.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/gabor.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/gabor.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/generalized_hough.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/generalized_hough.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/geometry.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/geometry.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/grabcut.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/grabcut.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/hershey_fonts.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/hershey_fonts.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/histogram.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/histogram.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/hough.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/hough.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/imgwarp.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/imgwarp.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/intersection.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/intersection.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/linefit.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/linefit.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/lsd.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/lsd.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/main.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/main.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/matchcontours.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/matchcontours.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/min_enclosing_triangle.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/min_enclosing_triangle.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/moments.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/moments.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/morph.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/morph.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/phasecorr.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/phasecorr.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/pyramids.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/pyramids.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/rotcalipers.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/rotcalipers.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/samplers.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/samplers.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/segmentation.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/segmentation.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/shapedescr.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/shapedescr.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/smooth.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/smooth.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/spatialgradient.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/spatialgradient.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/subdivision2d.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/subdivision2d.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/sumpixels.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/sumpixels.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/tables.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/tables.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/templmatch.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/templmatch.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/thresh.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/thresh.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/undistort.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/undistort.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/src/utils.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  "__OPENCV_BUILD=1"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/eigen3"
+  "../modules/imgproc/include"
+  "../modules/imgproc/src"
+  "modules/imgproc"
+  "../modules/core/include"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_imgproc.so" "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_imgproc.so.3.3.0"
+  "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_imgproc.so.3.3" "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_imgproc.so.3.3.0"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/opencl_kernels_imgproc.hpp" "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/opencl_kernels_imgproc.cpp"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Packages/opencv-3.3.0/build/modules/core/CMakeFiles/opencv_core.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/3rdparty/carotene/hal/CMakeFiles/tegra_hal.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ opencv-3.3.0/build/modules/imgproc/opencv_perf_imgproc_pch_dephelp.cxx
@@ -0,0 +1,6 @@
+#include "/home/pi/Packages/opencv-3.3.0/modules/imgproc/perf/perf_precomp.hpp"
+int testfunction();
+int testfunction()
+{
+    return 0;
+}
