878b4b08c04771cb1fd73cbb1f85a354f8f4f048
pigeostake <cstarkjp@gmail.com>
2017-11-06 14:24:09 +0900

    Partial commit: videoio

---
+++ opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_test_videoio.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Packages/opencv-3.3.0/modules/videoio/test/test_ffmpeg.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_test_videoio.dir/test/test_ffmpeg.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/videoio/test/test_fourcc.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_test_videoio.dir/test/test_fourcc.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/videoio/test/test_main.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_test_videoio.dir/test/test_main.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/videoio/test/test_mfx.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_test_videoio.dir/test/test_mfx.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/videoio/test/test_video_io.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_test_videoio.dir/test/test_video_io.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  "__OPENCV_BUILD=1"
+  "__OPENCV_TESTS=1"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/eigen3"
+  "/usr/include/arm-linux-gnueabihf"
+  "../modules/ts/include"
+  "../modules/videoio/include"
+  "../modules/imgcodecs/include"
+  "../modules/core/include"
+  "../modules/imgproc/include"
+  "../modules/highgui/include"
+  "../modules/videoio/test"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Packages/opencv-3.3.0/build/modules/ts/CMakeFiles/opencv_ts.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/highgui/CMakeFiles/opencv_highgui.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_videoio.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_imgcodecs.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/core/CMakeFiles/opencv_core.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/3rdparty/carotene/hal/CMakeFiles/tegra_hal.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
