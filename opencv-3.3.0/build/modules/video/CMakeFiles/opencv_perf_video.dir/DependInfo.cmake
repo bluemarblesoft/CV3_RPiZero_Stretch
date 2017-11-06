ca6dedd66016fa661aae370cbe4320860d9a14ac
pigeostake <cstarkjp@gmail.com>
2017-11-06 14:18:58 +0900

    Partial commit: video

---
+++ opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/opencl/perf_bgfg_mog2.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/opencl/perf_bgfg_mog2.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/opencl/perf_motempl.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/opencl/perf_motempl.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/opencl/perf_optflow_dualTVL1.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/opencl/perf_optflow_dualTVL1.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/opencl/perf_optflow_farneback.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/opencl/perf_optflow_farneback.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/opencl/perf_optflow_pyrlk.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/opencl/perf_optflow_pyrlk.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/perf_ecc.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/perf_ecc.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/perf_main.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/perf_main.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/perf_optflowpyrlk.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/perf_optflowpyrlk.cpp.o"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/perf/perf_tvl1optflow.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_perf_video.dir/perf/perf_tvl1optflow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  "__OPENCV_BUILD=1"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/eigen3"
+  "../modules/ts/include"
+  "../modules/video/include"
+  "../modules/imgcodecs/include"
+  "../modules/core/include"
+  "../modules/imgproc/include"
+  "../modules/videoio/include"
+  "../modules/highgui/include"
+  "../modules/video/perf"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Packages/opencv-3.3.0/build/modules/ts/CMakeFiles/opencv_ts.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_video.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/highgui/CMakeFiles/opencv_highgui.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_videoio.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_imgcodecs.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/3rdparty/carotene/hal/CMakeFiles/tegra_hal.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/core/CMakeFiles/opencv_core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ opencv-3.3.0/build/modules/video/opencv_perf_video_pch_dephelp.cxx
@@ -0,0 +1,6 @@
+#include "/home/pi/Packages/opencv-3.3.0/modules/video/perf/perf_precomp.hpp"
+int testfunction();
+int testfunction()
+{
+    return 0;
+}
