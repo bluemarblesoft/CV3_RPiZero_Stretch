7e3c10af5ab6ea89f2bd05b36a913b735299afa8
pigeostake <cstarkjp@gmail.com>
2017-11-06 12:00:48 +0900

    Partial commit: contrib_world

---
+++ opencv-3.3.0/build/modules/contrib_world/.contrib_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Packages/opencv_contrib-3.3.0/modules/contrib_world
+# Build directory: /home/pi/Packages/opencv-3.3.0/build/modules/contrib_world/.contrib_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
