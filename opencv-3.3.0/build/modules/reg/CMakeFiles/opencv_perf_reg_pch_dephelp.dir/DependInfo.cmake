69d25530a3f079c39b17de235db6e3ecb74c0af3
pigeostake <cstarkjp@gmail.com>
2017-11-06 13:28:51 +0900

    Partial commit: reg

---
+++ opencv-3.3.0/build/modules/reg/CMakeFiles/opencv_perf_reg_pch_dephelp.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Packages/opencv-3.3.0/build/modules/reg/opencv_perf_reg_pch_dephelp.cxx" "/home/pi/Packages/opencv-3.3.0/build/modules/reg/CMakeFiles/opencv_perf_reg_pch_dephelp.dir/opencv_perf_reg_pch_dephelp.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  "__OPENCV_BUILD=1"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/eigen3"
+  "/home/pi/Packages/opencv-3.3.0/modules/ts/include"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/reg/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/core/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/videoio/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/highgui/include"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/reg/perf"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
