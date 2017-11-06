4b21360abc2dcf573ffc5bc6d701b83f34dfac2e
pigeostake <cstarkjp@gmail.com>
2017-11-06 14:33:17 +0900

    Partial commit: xfeatures2d

---
+++ opencv-3.3.0/build/modules/xfeatures2d/.xfeatures2d/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d
+# Build directory: /home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/.xfeatures2d
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/opencl_kernels_xfeatures2d.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/opencl_kernels_xfeatures2d.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/affine_feature2d.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/affine_feature2d.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/boostdesc.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/boostdesc.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/brief.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/brief.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/daisy.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/daisy.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/ellipticKeyPoint.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/ellipticKeyPoint.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/freak.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/freak.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/harris_lapace_detector.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/harris_lapace_detector.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/latch.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/latch.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/lucid.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/lucid.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/msd.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/msd.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/pct_signatures.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/pct_signatures.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/pct_signatures/grayscale_bitmap.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/pct_signatures/grayscale_bitmap.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/pct_signatures/pct_clusterizer.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/pct_signatures/pct_clusterizer.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/pct_signatures/pct_sampler.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/pct_signatures/pct_sampler.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/pct_signatures_sqfd.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/pct_signatures_sqfd.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/sift.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/sift.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/stardetector.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/stardetector.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/surf.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/surf.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/surf.cuda.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/surf.cuda.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/surf.ocl.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/surf.ocl.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/vgg.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/vgg.cpp.o"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src/xfeatures2d_init.cpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/CMakeFiles/opencv_xfeatures2d.dir/src/xfeatures2d_init.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  "__OPENCV_BUILD=1"
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/eigen3"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/include"
+  "/home/pi/Packages/opencv_contrib-3.3.0/modules/xfeatures2d/src"
+  "modules/xfeatures2d"
+  "/home/pi/Packages/opencv-3.3.0/modules/core/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/flann/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgproc/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/video/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/imgcodecs/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/shape/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/videoio/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/highgui/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/features2d/include"
+  "/home/pi/Packages/opencv-3.3.0/modules/calib3d/include"
+  "downloads/xfeatures2d"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_xfeatures2d.so" "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_xfeatures2d.so.3.3.0"
+  "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_xfeatures2d.so.3.3" "/home/pi/Packages/opencv-3.3.0/build/lib/libopencv_xfeatures2d.so.3.3.0"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/opencl_kernels_xfeatures2d.hpp" "/home/pi/Packages/opencv-3.3.0/build/modules/xfeatures2d/opencl_kernels_xfeatures2d.cpp"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Packages/opencv-3.3.0/build/modules/shape/CMakeFiles/opencv_shape.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/calib3d/CMakeFiles/opencv_calib3d.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/3rdparty/carotene/hal/CMakeFiles/tegra_hal.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/video/CMakeFiles/opencv_video.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/features2d/CMakeFiles/opencv_features2d.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/flann/CMakeFiles/opencv_flann.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/highgui/CMakeFiles/opencv_highgui.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/videoio/CMakeFiles/opencv_videoio.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgcodecs/CMakeFiles/opencv_imgcodecs.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/imgproc/CMakeFiles/opencv_imgproc.dir/DependInfo.cmake"
+  "/home/pi/Packages/opencv-3.3.0/build/modules/core/CMakeFiles/opencv_core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
