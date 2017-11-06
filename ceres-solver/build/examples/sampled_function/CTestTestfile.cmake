4248b67ff9c92f8a7edfc7c1c7baf51e2f71b175
pigeostake <cstarkjp@gmail.com>
2017-11-06 10:58:15 +0900

    Piecemeal pushing

---
+++ ceres-solver/build/examples/sampled_function/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Packages/ceres-solver/examples/sampled_function
+# Build directory: /home/pi/Packages/ceres-solver/build/examples/sampled_function
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ceres-solver/build/internal/ceres/CMakeFiles/test_util.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Packages/ceres-solver/internal/ceres/evaluator_test_utils.cc" "/home/pi/Packages/ceres-solver/build/internal/ceres/CMakeFiles/test_util.dir/evaluator_test_utils.cc.o"
+  "/home/pi/Packages/ceres-solver/internal/ceres/numeric_diff_test_utils.cc" "/home/pi/Packages/ceres-solver/build/internal/ceres/CMakeFiles/test_util.dir/numeric_diff_test_utils.cc.o"
+  "/home/pi/Packages/ceres-solver/internal/ceres/test_util.cc" "/home/pi/Packages/ceres-solver/build/internal/ceres/CMakeFiles/test_util.dir/test_util.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_CXSPARSE_VERSION=\"3.1.9\""
+  "CERES_GFLAGS_NAMESPACE=google"
+  "CERES_SUITESPARSE_VERSION=\"4.5.4\""
+  "EIGEN_MPL2_ONLY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "config"
+  "../include"
+  "../internal"
+  "../internal/ceres"
+  "/usr/include/eigen3"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Packages/ceres-solver/build/internal/ceres/CMakeFiles/ceres.dir/DependInfo.cmake"
+  "/home/pi/Packages/ceres-solver/build/internal/ceres/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
